fe81f5dc24bf8c21fa5d551aa3ded802e0154271
Aleksei Samoilov <lek-z@yandex.ru>
2025-06-26 13:02:24 +0200

    codeDisplay component added

--- src/components/CodeDisplay/CodeDisplay.tsx
+++ src/components/CodeDisplay/CodeDisplay.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import styles from './CodeDisplay.module.scss'
 
-const CodeDisplay = () => {
+interface ICodeDisplayProps {
+    code: string;
+    className?: string
+}
+
+const CodeDisplay: React.FC<ICodeDisplayProps> = ({ code, className }) => {
+    const highlightSyntax = (code: string): string => {
+        return code
+            .replace(/(let|const|var|function|if|else|for|while|do|switch|return|try|catch|new|this|typeof)/g, '<span class="keyword">$1</span>')
+    }
     return (
         <div>
 
