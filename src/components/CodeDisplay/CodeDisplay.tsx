9be1fb0b3bbba3e04a28a6f4d268b22bfe9bf75a
Aleksei Samoilov <lek-z@yandex.ru>
2025-06-28 11:31:52 +0200

    feat: minor improve hilight syntax fucntion

--- src/components/CodeDisplay/CodeDisplay.tsx
+++ src/components/CodeDisplay/CodeDisplay.tsx
@@ -10,6 +10,11 @@ const CodeDisplay: React.FC<ICodeDisplayProps> = ({ code, className }) => {
     const highlightSyntax = (code: string): string => {
         return code
             .replace(/(let|const|var|function|if|else|for|while|do|switch|return|try|catch|new|this|typeof)/g, '<span class="keyword">$1</span>')
+            .replace(/([0-9]+)/g, '<span class="number">$1</span>')
+            .replace(/(["'`])(.*?)\1/g, '<span class="string">$1$2$1</span>')
+            .replace(/(\/\/.*)$/gm, '<span class="comment">$1</span>')
+            .replace(/(console\.log)/g, '<span class="console">$1</span>')
+            .replace(/(setTimeout|Promise)/g, '<span class="async">$1</span>');
     }
     return (
         <div>
